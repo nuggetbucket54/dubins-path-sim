4eabcfa90f45fad47274d51a149c7f4be63e2c6b
nuggetbucket54 <ryan.zy.cheng@gmail.com>
2024-02-10 15:53:44 -0800

    tweak plot

--- dubins.py
+++ dubins.py
@@ -6,22 +6,24 @@ width=100
 height=80
 turningRadius = 10
 
-def genCoords():
-    """generates two random coordinates representing drone position and waypoint"""
+# generates two random coordinates representing drone position and waypoint
+def genCoords():  
     return [[random.randint(1,width-1),random.randint(1,height-1)],
             [random.randint(1,width-1),random.randint(1,height-1)]]
 
-
+# plot settings
 fig, ax = plt.subplots(figsize=(8,6))
+ax.set_xlim(0,width)
+ax.set_ylim(0,height)
+ax.set_title('Dubins path sim')
+
+
+# plot points
 m,n = genCoords()
 ax.plot(m[0], m[1], 'ro', label='Point 1')
-ax.plot(n[0], n[1], 'ro', label='Point 2')
+ax.plot(n[0], n[1], 'bo', label='Point 2')
 
-# Add labels and legend
-ax.set_xlabel('X-axis')
-ax.set_ylabel('Y-axis')
-ax.set_title('Plotting Two Points')
-ax.legend()
+
 
 # Display the plot
 plt.show()
