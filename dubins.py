411f1282d7ee66f2664d01a8f118727d45091a0b
nuggetbucket54 <ryan.zy.cheng@gmail.com>
2024-02-11 01:43:34 -0800

    working dubin path for basic test

--- dubins.py
+++ dubins.py
@@ -1,13 +1,13 @@
 import random, math
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
+from matplotlib.patches import Circle, Arc
 from matplotlib.widgets import Button
 from typing import Tuple
 
 # constants (feel free to play around and change these values)
 width=100
 height=80
-turningRadius = 10
+turningRadius = 5
 
 dronePos: Tuple[int, int]
 droneVec: Tuple[float, float]
@@ -49,7 +49,7 @@ def draw(ax):
     ax.clear()
     ax.set_xlim(0,width)
     ax.set_ylim(0,height)
-    ax.set_aspect('equal')
+    ax.set_aspect('equal', adjustable='box')
     ax.set_title('Dubins path sim')
 
     ax.plot(dronePos[0], dronePos[1], 'bo', label='Point 1')
@@ -60,13 +60,13 @@ def draw(ax):
 
 # plot settings
 fig, ax = plt.subplots(figsize=(6,6))
-plt.subplots_adjust(bottom=0.25)
+plt.subplots_adjust(bottom=0.2)
 
 
 dronePos = [20,20]
 pointPos = [80,40]
 droneVec = [0,1]
-pointVec = [0,1]
+pointVec = [0,-1]
 # droneVec = [0.5,0.75**.5]
 # pointVec = [0.5,-0.75**.5]
 
@@ -74,8 +74,6 @@ draw(ax)
 
 p1 = [dronePos[0] + droneVec[1]*turningRadius, dronePos[1]-droneVec[0]*turningRadius]
 p2 = [pointPos[0] - pointVec[1]*turningRadius, pointPos[1]-pointVec[0]*turningRadius]
-c1 = Circle((p1[0], p1[1]), turningRadius, fill=False, color='blue')
-c2 = Circle((p2[0], p2[1]), turningRadius, fill=False, color='blue')
 
 V = [p2[0]-p1[0], p2[1]-p1[1]]
 D = (V[0]**2 + V[1]**2)**.5
@@ -85,16 +83,29 @@ angle = math.acos(2*turningRadius/D) + math.atan2(V[1],V[0])
 pf1 = [p1[0] + turningRadius * math.cos(angle), p1[1] + turningRadius * math.sin(angle)]
 pf2 = [p2[0] - turningRadius * math.cos(angle), p2[1] - turningRadius * math.sin(angle)]
 
-ax.plot(p1[0], p1[1], 'bo', label='Point 1')
-ax.plot(p2[0], p2[1], 'ro', label='Point 2')
-ax.plot(pf1[0], pf1[1], 'bo', label='Point 1')
-ax.plot(pf2[0], pf2[1], 'ro', label='Point 2')
-ax.add_patch(c1)
-ax.add_patch(c2)
+curveAng1A = math.atan2(dronePos[1]-p1[1], dronePos[0]-p1[0])
+curveAng1B = math.atan2(pf1[1]-p1[1], pf1[0]-p1[0])
+
+curveAng2A = math.atan2(pointPos[1]-p2[1], pointPos[0]-p2[0])
+curveAng2B = math.atan2(pf2[1]-p2[1], pf2[0]-p2[0])
+
+curve1 = Arc((p1[0],p1[1]), 2*turningRadius, 2*turningRadius, theta1=curveAng1B*180/math.pi, theta2=curveAng1A*180/math.pi, color='purple', linewidth=1)
+curve2 = Arc((p2[0],p2[1]), 2*turningRadius, 2*turningRadius, theta1=curveAng2B*180/math.pi, theta2=curveAng2A*180/math.pi, color='purple', linewidth=1)
+
+ax.add_patch(curve1)
+ax.add_patch(curve2)
+ax.plot([pf1[0], pf2[0]], [pf1[1], pf2[1]], color='purple', linewidth=1)
+
+# listed below are points/lines used for calculations that can be visualized if wanted
+# ax.plot(p1[0], p1[1], 'bo', label='Point 1')
+# ax.plot(p2[0], p2[1], 'ro', label='Point 2')
+# ax.plot(pf1[0], pf1[1], 'bo', label='Point 1')
+# ax.plot(pf2[0], pf2[1], 'ro', label='Point 2')
+# ax.add_patch(c1)
+# ax.add_patch(c2)
 
 
-
-button_ax = plt.axes([0.05, 0.05, 0.9, 0.1])  # [left, bottom, width, height]
+button_ax = plt.axes([0.05, 0.05, 0.9, 0.08])  # [left, bottom, width, height]
 button = Button(button_ax, 'Generate next waypoint')
 button.on_clicked(nextPoint)
 
