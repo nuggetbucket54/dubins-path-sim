412476cade3eaef98cfd9e3d901dbbee9627d4a0
Ryan Cheng <ryan.zy.cheng@gmail.com>
2024-02-14 15:03:35 -0800

    figured out math for LSL

--- dubins.py
+++ dubins.py
@@ -2,18 +2,12 @@ import random, math
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Arc
 from matplotlib.widgets import Button
-from typing import Tuple
 
 # constants (feel free to play around and change these values)
 width=100
 height=80
 TURNRADIUS = 5
 PI = math.pi
-
-dronePos: Tuple[int, int]
-droneVec: Tuple[float, float]
-pointPos: Tuple[int, int]
-pointVec: Tuple[float, float]
 
 # generates two random coordinates representing drone position and waypoint
 # (coordinates generated at least 10 units from edge of plot for easier visualization)
@@ -74,7 +68,8 @@ def drawPath(ax):
     pointLeft = [pointPos[0] + TURNRADIUS * math.cos(pointLeftAngle), pointPos[1] + TURNRADIUS * math.sin(pointLeftAngle)]
     pointRight = [pointPos[0] + TURNRADIUS * math.cos(pointRightAngle), pointPos[1] + TURNRADIUS * math.sin(pointRightAngle)]
 
-    RSR(droneRight, pointRight, dronePos, pointPos)
+    # RSR(droneRight, pointRight, dronePos, pointPos)
+    LSL(droneLeft, pointLeft, dronePos, pointPos)
 
     # listed below are points/lines used for calculations that can be visualized if wanted
     # ax.plot(droneLeft[0], droneLeft[1], 'bo', label='Point 1')
@@ -86,9 +81,8 @@ def drawPath(ax):
     # ax.add_patch(c1)
     # ax.add_patch(c2)
 
-
+# path for right-straight-right path
 def RSR(p1, p2, dronePos, pointPos):
-    # vector between two focii of circles
     V = [p2[0] - p1[0], p2[1] - p1[1]]
     D = (V[0]**2 + V[1]**2)**.5
 
@@ -116,6 +110,35 @@ def RSR(p1, p2, dronePos, pointPos):
     ax.add_patch(curve2)
     ax.plot([pf1[0], pf2[0]], [pf1[1], pf2[1]], color='purple', linewidth=1)
 
+# path for left-straight-left path
+def LSL(p1, p2, dronePos, pointPos):
+    V = [p2[0] - p1[0], p2[1] - p1[1]]
+    D = (V[0]**2 + V[1]**2)**.5
+
+    angle = math.acos(2*TURNRADIUS/D) + math.atan2(V[1],V[0])
+
+    xdiff = TURNRADIUS * math.cos(angle)
+    ydiff = TURNRADIUS * math.sin(angle)
+
+    # tangent points of circles
+    pf1 = [p1[0] - xdiff, p1[1] - ydiff]
+    pf2 = [p2[0] - xdiff, p2[1] - ydiff]
+
+    # angle calculations for drawing curves
+    curveAng1A = math.atan2(dronePos[1]-p1[1], dronePos[0]-p1[0])
+    curveAng1B = math.atan2(pf1[1]-p1[1], pf1[0]-p1[0])
+
+    curveAng2A = math.atan2(pointPos[1]-p2[1], pointPos[0]-p2[0])
+    curveAng2B = math.atan2(pf2[1]-p2[1], pf2[0]-p2[0])
+
+    curve1 = Arc((p1[0],p1[1]), 2*TURNRADIUS, 2*TURNRADIUS, theta1=curveAng1A*180/PI, theta2=curveAng1B*180/PI, color='purple', linewidth=1)
+    curve2 = Arc((p2[0],p2[1]), 2*TURNRADIUS, 2*TURNRADIUS, theta1=curveAng2B*180/PI, theta2=curveAng2A*180/PI, color='purple', linewidth=1)
+
+    # add curves to graph
+    ax.add_patch(curve1)
+    ax.add_patch(curve2)
+    ax.plot([pf1[0], pf2[0]], [pf1[1], pf2[1]], color='purple', linewidth=1)
+
 # redraws canvas
 def draw(ax):
     ax.clear()
@@ -135,10 +158,10 @@ def draw(ax):
 fig, ax = plt.subplots(figsize=(6,6))
 plt.subplots_adjust(bottom=0.2)
 
-dronePos = [80,20]
-pointPos = [20,20]
-droneVec = [0,1]
-pointVec = [0,1]
+dronePos = genCoords()
+pointPos = genCoords()
+droneVec = genVec()
+pointVec = genVec()
 
 draw(ax)
 
