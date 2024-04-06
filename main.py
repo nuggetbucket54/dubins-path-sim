265e7e26d73b0b12dbc5005438e3065a9927441c
Ryan Cheng <ryan.zy.cheng@gmail.com>
2024-04-06 15:22:51 -0700

    update docs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dubins import dubin
 
 app = FastAPI()
 
+# waypoint object
 class Waypoint(BaseModel):
     id: int
     name: str
@@ -11,14 +12,14 @@ class Waypoint(BaseModel):
     long: float
     alt: float
 
-
+# object received from GCOM
 class Item(BaseModel):
     current_waypoint: Waypoint
     desired_waypoint: Waypoint
     current_heading: float
     desired_heading: float
 
-
+# converts calculated latitude/longitude into JSON format to match object
 def create_item(lat, long, alt):
     return {
         "id": -1,
@@ -30,6 +31,7 @@ def create_item(lat, long, alt):
 
 @app.post("/")
 async def root(item: Item):
+    # get drone and waypoint positions
     drone = item.current_waypoint
     destination = item.desired_waypoint
     alt = drone.alt
@@ -43,14 +45,11 @@ async def root(item: Item):
     point_long = destination.long
     point_angle = item.desired_heading
 
+    # grab points of Dubins path
     points = dubin(drone_lat, drone_long, drone_angle, point_lat, point_long, point_angle)
 
+    # turn all points into JSON objects
     for p in points:
         waypoints.append(create_item(p[0], p[1], alt))
-
-    # print('\n'*10)
-    # for w in waypoints:
-    #     print(str(w["lat"])+","+str(w["long"]))
-    # print('\n'*10)
 
     return {"waypoints": waypoints}
